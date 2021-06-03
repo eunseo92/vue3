0ce17274de7dc68877815c4c81f8fb98601cbebf
Eunseo Park <eunseopark@Eunseos-MacBook-Pro.local>
2021-06-03 20:58:03 +0900

    image path modified

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: 'shanai01@outlook.kr',
     github: 'https://github.com/eunseo92',
     phone: '+82-10-3123-9500',
-    image: 'src/assets/Eunseo.png'
+    image: 'https://github.com/eunseo92/vue-movie-app/blob/96394577df0e7851b613a6ff0af6ed337e4933df/src/assets/Eunseo.png'
   })
 }

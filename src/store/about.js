893292246c57800a71073b2c363644f3e50db806
Eunseo Park <eunseopark@Eunseos-MacBook-Pro.local>
2021-06-10 15:18:34 +0900

    image hosted

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: 'shanai01@outlook.kr',
     github: 'https://github.com/eunseo92',
     phone: '+82-10-3123-9500',
-    image: 'https://blog.naver.com/common/util/imageZoom.jsp?url=https://blogpfthumb-phinf.pstatic.net/MjAyMTA2MDdfNzIg/MDAxNjIzMDQxMTI2ODg4.yHe7mIz3b9Z8wpVo8Hkj_cXvffw7OHJSUO7IrB7GrpMg.WN8lDMmBQ00Hf2DBGSuZE6UFPEqC0PzCJ2aYXJV2c1Ug.PNG.shanai01/Eunseo.png&rClickYn=false&isOwner=true'
+    image: 'https://ifh.cc/g/fxBLLL.jpg'
   })
 }
